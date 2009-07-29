b26455392924bd72e25acfc16c4ccd9410933f33
Andre Van Der Merwe <dart@pobox.com>
2009-07-29 22:37:35 +0200

    WPF boo example redirects print output

--- WinFormsTestApp/Form1.cs
+++ WinFormsTestApp/Form1.cs
@@ -12,60 +12,83 @@ using Boo.Lang.Interpreter;
 
 using TerminalCore;
 using TerminalCore.Model;
+using System.IO;
 
 namespace WinFormsTestApp
 {
-	public partial class Form1 : Form
-	{
-		private InteractiveInterpreter2 m_boo;
+   public partial class Form1 : Form
+   {
+      private InteractiveInterpreter2 m_boo;
+      private DummyTextWriter m_writer = new DummyTextWriter();
 
-		public Form1()
-		{
-			InitializeComponent();
-		}
+      public Form1()
+      {
+         InitializeComponent();
+      }
 
-		protected override void OnLoad( EventArgs e )
-		{
-			base.OnLoad( e );
+      protected override void OnLoad( EventArgs e )
+      {
+         base.OnLoad( e );
 
-			m_boo = new InteractiveInterpreter2();
-			m_boo.RememberLastValue = true;
+         m_boo = new InteractiveInterpreter2();
+         m_boo.RememberLastValue = true;
 
-			m_terminal.Terminal.LineEntered += Terminal_LineEntered;
-			m_terminal.Terminal.ControlCharEntered += Terminal_ControlCharEntered;
-		}
+         m_writer.OnWrite = s => m_terminal.Terminal.WriteOutput( s );
+         Console.SetOut( m_writer );
 
-		private void Terminal_ControlCharEntered( object sender, CharEventArgs e )
-		{
-			if( e.Char == '\t' )
-			{
-				//m_boo.SuggestCodeCompletion(  )
-				//TODO suggest completion
-			}
-		}
+         m_terminal.Terminal.LineEntered += Terminal_LineEntered;
+         m_terminal.Terminal.ControlCharEntered += Terminal_ControlCharEntered;
+      }
 
-		private void Terminal_LineEntered( object sender, LineEventArgs e )
-		{
-			try
-			{
-				CompilerContext ctx = m_boo.Eval( e.Line );
+      private void Terminal_ControlCharEntered( object sender, CharEventArgs e )
+      {
+         if( e.Char == '\t' )
+         {
+            //m_boo.SuggestCodeCompletion(  )
+            //TODO suggest completion
+         }
+      }
 
-				if( ctx.Errors.Count == 0 )
-				{
-					if( m_boo.LastValue != null )
-					{
-						m_terminal.Terminal.WriteOutput( m_boo.LastValue.ToString(), Colours.Green );
-					}
-				}
-				else
-				{
-					m_terminal.Terminal.WriteOutput( ctx.Errors[ 0 ].Message, Colours.Red );
-				}
-			}
-			catch( Exception ex )
-			{
-				m_terminal.Terminal.WriteOutput( ex.Message, Colours.Red );
-			}
-		}
-	}
+      private void Terminal_LineEntered( object sender, LineEventArgs e )
+      {
+         try
+         {
+            CompilerContext ctx = m_boo.Eval( e.Line );
+
+            if( ctx.Errors.Count == 0 )
+            {
+               if( m_boo.LastValue != null )
+               {
+                  m_terminal.Terminal.WriteOutput( m_boo.LastValue.ToString(), Colours.Green );
+               }
+            }
+            else
+            {
+               m_terminal.Terminal.WriteOutput( ctx.Errors[ 0 ].Message, Colours.Red );
+            }
+         }
+         catch( Exception ex )
+         {
+            m_terminal.Terminal.WriteOutput( ex.Message, Colours.Red );
+         }
+      }
+
+      class DummyTextWriter : TextWriter
+      {
+         public Action<string> OnWrite { get; set; }
+
+         public override void Write( char[] buffer, int index, int count )
+         {
+            var txt = new StringWriter();
+            txt.Write( buffer, index, count );
+
+            if( OnWrite != null )
+            {
+               OnWrite( txt.ToString() );
+            }
+         }
+
+         public override Encoding Encoding { get { return Encoding.ASCII; } }
+      }
+   }
 }
